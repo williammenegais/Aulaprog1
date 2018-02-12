979380ca73b47843ee15389b9a5103b5ac890b56
williammenegais <36333482+williammenegais@users.noreply.github.com>
2018-02-12 13:05:00 -0200

    Fist commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: willi
+ *
+ * Created on 12 de Fevereiro de 2018, 13:01
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
